07ca497ced1877d7478b6d796564fcd30795564e
PSW <woemtis@naver.com>
2019-07-22 12:12:57 +0900

    network Client

---
+++ Assets/Project/Scripts/NetworkScripts/NetworkClient.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class NetworkClient : MonoBehaviour
+{
+    Socket socket;
+    IPEndPoint ep;
+    string serverIP = "127.0.0.1";
+    int serverPort = 9876;
+    //byte[] sendBuff = new byte[1024];
+    byte[] sendBuff = Encoding.UTF8.GetBytes("HIHI!!");
+    byte[] recvBuff = new byte[1024];
+
+    void Start()
+    {
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        ep = new IPEndPoint(IPAddress.Parse(serverIP), serverPort);
+        socket.Connect(ep);
+    }
+
+    void Update()
+    {
+        socket.Send(sendBuff, SocketFlags.None);
+
+        int n = socket.Receive(recvBuff);
+        print("Recv byte = " + n);
+        print(Encoding.UTF8.GetString(recvBuff, 0, n));
+    }
+}
