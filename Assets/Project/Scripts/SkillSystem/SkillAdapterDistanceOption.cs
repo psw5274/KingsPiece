5abe7928e384c438cec5a25735e55ac5386d506e
Tetramad <cbwjd777@naver.com>
2019-08-16 00:36:04 +0900

    추가 : 거리에 따라 다른 효과를 주는 SkillDistanceOption 완성

--- Assets/Project/Scripts/SkillSystem/SkillAdapterDistanceOption.cs
+++ Assets/Project/Scripts/SkillSystem/SkillAdapterDistanceOption.cs
@@ -1,11 +1,69 @@
+using System;
+using System.Linq;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Skill Adapter", menuName = "Skill Adapter", order = 0)]
 class SkillAdapterDistanceOption : Skill
 {
+    [Serializable]
+    private class DistanceOption : IComparable<DistanceOption>
+    {
+        public enum CompareType
+        {
+            Equal,
+            Greater,
+            Lesser,
+            GreaterAndEqual,
+            LesserAndEqual
+        }
+        public int distance;
+        public CompareType comparer;
+        public Skill skill;
+
+        public int CompareTo(DistanceOption other)
+        {
+            return distance.CompareTo(other.distance);
+        }
+    }
+
+    [SerializeField]
+    private DistanceOption[] options;
+
     public override void Operate(BoardCoord selectedBoardCoord)
     {
-        var target = BoardManager.Instance.boardStatus[selectedBoardCoord.col][selectedBoardCoord.row].GetComponent<Piece>();
-        EffectManager.Instance.AddEffect(target, this);
+        Piece piece = BoardManager.Instance.selectedPiece.GetComponent<Piece>();
+        Piece target = BoardManager.Instance.boardStatus[selectedBoardCoord.col][selectedBoardCoord.row].GetComponent<Piece>();
+
+        int distance = BoardCoord.Distance(piece.pieceCoord, target.pieceCoord);
+
+        var selectedOptions = options.Where(distanceOption => DistanceCompare(distanceOption, distance));
+
+        if (selectedOptions.Count() == 0)
+        {
+            EffectManager.Instance.AddEffect(target, this);
+            return;
+        }
+
+        var option = selectedOptions.Max();
+        EffectManager.Instance.AddEffect(target, option.skill);
+    }
+
+    private bool DistanceCompare(DistanceOption option, int distance)
+    {
+        switch (option.comparer)
+        {
+            case DistanceOption.CompareType.Equal:
+                return option.distance == distance;
+            case DistanceOption.CompareType.Greater:
+                return option.distance > distance;
+            case DistanceOption.CompareType.GreaterAndEqual:
+                return option.distance >= distance;
+            case DistanceOption.CompareType.Lesser:
+                return option.distance < distance;
+            case DistanceOption.CompareType.LesserAndEqual:
+                return option.distance <= distance;
+            default:
+                return false;
+        }
     }
 }
